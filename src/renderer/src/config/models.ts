0f5669036eb1c5d4b20ae57599682b13084c4154
ousugo <dkzyxh@gmail.com>
2025-02-27 11:22:27 +0800

    feat: Added resoning model recognition support for Claude 3.7 Sonnet model

--- src/renderer/src/config/models.ts
+++ src/renderer/src/config/models.ts
@@ -164,7 +164,7 @@ export const VISION_REGEX = new RegExp(
 )
 
 export const TEXT_TO_IMAGE_REGEX = /flux|diffusion|stabilityai|sd-|dall|cogview|janus/i
-export const REASONING_REGEX = /^(o\d+(?:-[\w-]+)?|.*\b(?:reasoner|thinking)\b.*|.*-[rR]\d+.*)$/i
+export const REASONING_REGEX = /^(o\d+(?:-[\w-]+)?|.*\b(?:reasoner|thinking)\b.*|.*-[rR]\d+.*|.*claude-3-7-sonnet.*)$/i
 
 export const EMBEDDING_REGEX = /(?:^text-|embed|bge-|e5-|LLM2Vec|retrieval|uae-|gte-|jina-clip|jina-embeddings)/i
 export const NOT_SUPPORTED_REGEX = /(?:^tts|rerank|whisper|speech)/i
@@ -1832,6 +1832,14 @@ export function isReasoningModel(model: Model): boolean {
   }
 
   return REASONING_REGEX.test(model.id) || model.type?.includes('reasoning') || false
+}
+
+export function isClaudeThinkingModel(model: Model): boolean {
+  if (!model) {
+    return false
+  }
+
+  return model?.id?.includes('claude-3-7-sonnet') || false
 }
 
 export function isSupportedModel(model: OpenAI.Models.Model): boolean {
