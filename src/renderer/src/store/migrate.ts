1bae626da9736158d234762510e04ff81a394185
eeee0717 <chentao020717Work@outlook.com>
2025-03-03 20:01:09 +0800

    chore: add old version migrate

--- src/renderer/src/store/migrate.ts
+++ src/renderer/src/store/migrate.ts
@@ -1214,6 +1214,13 @@ const migrateConfig = {
       isSystem: true,
       enabled: false
     })
+    const existWebsearchProvider = state.websearch.providers.find((p) => p.id === 'tavily')
+    if (existWebsearchProvider && existWebsearchProvider.apiKey !== '') {
+      state.websearch.providers.push({
+        ...existWebsearchProvider,
+        enabled: true
+      })
+    }
     state.websearch.providers.push({
       id: 'searxng',
       name: 'Searxng',
