8efa51f97dc95d000881ea0844959e55e77bb792
one <wangan.cs@gmail.com>
2025-04-04 16:19:28 +0800

    fix: update migrate version

--- src/renderer/src/store/index.ts
+++ src/renderer/src/store/index.ts
@@ -42,7 +42,7 @@ const persistedReducer = persistReducer(
   {
     key: 'cherry-studio',
     storage,
-    version: 89,
+    version: 90,
     blacklist: ['runtime', 'messages'],
     migrate
   },
