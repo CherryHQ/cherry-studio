3142c33f019dde139a277ee3e82617c2bc1a527c
beyondkmp <beyondkmp@gmail.com>
2025-04-16 18:39:13 +0800

    refactor(electron-builder): Update artifact build script reference and remove obsolete after-build script
    - Changed the artifactBuildCompleted script reference in electron-builder.yml to point to the new script.
    - Deleted the outdated after-build.js script, which is no longer needed for file handling.

--- scripts/artifact-build-completed.js
+++ scripts/artifact-build-completed.js
@@ -2,21 +2,21 @@ const fs = require('fs')
 
 exports.default = function (buildResult) {
   try {
-    console.log('[After build] rename artifact file...')
+    console.log('[artifact build completed] rename artifact file...')
     if (!buildResult.file.includes(' ')) {
       return
     }
 
     let oldFilePath = buildResult.file
     if (oldFilePath.includes('-portable') && !oldFilePath.includes('-x64') && !oldFilePath.includes('-arm64')) {
-      console.log('[After build] delete portable file:', oldFilePath)
+      console.log('[artifact build completed] delete portable file:', oldFilePath)
       fs.unlinkSync(oldFilePath)
       return
     }
     const newfilePath = oldFilePath.replace(/ /g, '-')
     fs.renameSync(oldFilePath, newfilePath)
     buildResult.file = newfilePath
-    console.log(`[After build] rename file ${oldFilePath} to ${newfilePath} `)
+    console.log(`[artifact build completed] rename file ${oldFilePath} to ${newfilePath} `)
   } catch (error) {
     console.error('Error renaming file:', error)
   }
