cbac5e1d8fa441940a02c5ea41a1512a631e9809
fullex <0xfullex@gmail.com>
2025-04-27 19:23:25 +0800

    fix: type def errors in original index.ts file

--- src/preload/preload.d.ts
+++ src/preload/preload.d.ts
@@ -2,6 +2,7 @@ import { ElectronAPI } from '@electron-toolkit/preload'
 
 import type { WindowApiType } from './index'
 
+/** you don't need to declare this in your code, it's automatically generated */
 declare global {
   interface Window {
     electron: ElectronAPI
