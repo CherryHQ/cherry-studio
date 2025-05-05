fa3344ef34ce7dd1c68d2317cae3bb2388770bc4
beyondkmp <beyondkmp@gmail.com>
2025-05-05 16:06:23 +0800

    refactor: add Linux support for margin adjustments in MinappPopupContainer and McpSettingsNavbar

--- src/renderer/src/components/MinApp/MinappPopupContainer.tsx
+++ src/renderer/src/components/MinApp/MinappPopupContainer.tsx
@@ -8,7 +8,7 @@ import {
   PushpinOutlined,
   ReloadOutlined
 } from '@ant-design/icons'
-import { isMac, isWindows } from '@renderer/config/constant'
+import { isLinux, isMac, isWindows } from '@renderer/config/constant'
 import { DEFAULT_MIN_APPS } from '@renderer/config/minapps'
 import { useBridge } from '@renderer/hooks/useBridge'
 import { useMinappPopup } from '@renderer/hooks/useMinappPopup'
@@ -421,7 +421,7 @@ const ButtonsGroup = styled.div`
   gap: 5px;
   -webkit-app-region: no-drag;
   &.windows {
-    margin-right: ${isWindows ? '130px' : 0};
+    margin-right: ${isWindows || isLinux ? '130px' : 0};
     background-color: var(--color-background-mute);
     border-radius: 50px;
     padding: 0 3px;
