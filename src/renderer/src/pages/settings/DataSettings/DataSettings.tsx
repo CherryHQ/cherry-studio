e931b264d4ec466c01e88769a04fee0c209df8e5
VM 96 <eov@88.com>
2025-05-13 18:23:00 +0800

    优雅的引用图标

--- src/renderer/src/pages/settings/DataSettings/DataSettings.tsx
+++ src/renderer/src/pages/settings/DataSettings/DataSettings.tsx
@@ -17,7 +17,8 @@ import { reset } from '@renderer/services/BackupService'
 import { AppInfo } from '@renderer/types'
 import { formatFileSize } from '@renderer/utils'
 import { Button, Typography } from 'antd'
-import { FileText, FolderCog, FolderInput } from 'lucide-react'
+import { FileText, FolderCog, FolderInput,Sparkle } from 'lucide-react'
+
 import { FC, useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import styled from 'styled-components'
@@ -63,10 +64,7 @@ const DataSettings: FC = () => {
         p-id="2965"></path>
     </svg>
   )
-  const AidesIcon = () => (
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sparkle-icon lucide-sparkle"><path d="M9.937 15.5A2 2 0 0 0 8.5 14.063l-6.135-1.582a.5.5 0 0 1 0-.962L8.5 9.936A2 2 0 0 0 9.937 8.5l1.582-6.135a.5.5 0 0 1 .963 0L14.063 8.5A2 2 0 0 0 15.5 9.937l6.135 1.581a.5.5 0 0 1 0 .964L15.5 14.063a2 2 0 0 0-1.437 1.437l-1.582 6.135a.5.5 0 0 1-.963 0z"/></svg>
 
-  );
   const menuItems = [
     { key: 'divider_0', isDivider: true, text: t('settings.data.divider.basic') },
     { key: 'data', title: 'settings.data.data.title', icon: <FolderCog size={16} /> },
@@ -110,7 +108,7 @@ const DataSettings: FC = () => {
     {
       key: 'agentssubscribe_url',
       title: 'agents.tag.agent',
-      icon: <AidesIcon />
+      icon: <Sparkle />
     },
   ]
 
