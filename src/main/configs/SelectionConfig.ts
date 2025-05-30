15ca9a2c75e19b9eba61e5f845f0fe2e108430c8
fullex <0xfullex@gmail.com>
2025-05-31 00:26:41 +0800

    fix

--- src/main/configs/SelectionConfig.ts
+++ src/main/configs/SelectionConfig.ts
@@ -3,12 +3,15 @@ interface IBlacklist {
   MAC?: string[]
 }
 
-/**
+/*************************************************************************
  * 注意：请不要修改此配置，除非你非常清楚其含义、影响和行为的目的
  * Note: Do not modify this configuration unless you fully understand its meaning, implications, and intended behavior.
- * ----------------
+ * -----------------------------------------------------------------------
+ * A predefined application filter list to include commonly used software
+ * that does not require text selection but may conflict with it, and disable them in advance.
+ *
  * Specification: must be all lowercase, need to accurately find the actual running program name
- */
+ *************************************************************************/
 export const SELECTION_PREDEFINED_BLACKLIST: IBlacklist = {
   WINDOWS: [
     'snipaste.exe',
