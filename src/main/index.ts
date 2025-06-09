a95e6f8cb238fc4d7c8daaa3c2c4262bdf6edac4
beyondkmp <beyondkmkp@gmail.com>
2025-06-09 08:35:49 +0800

    feat: enhance unresponsive renderer handling and crash reporting
    * Added support for collecting JavaScript call stacks from unresponsive renderers.
    * Updated the Document Policy in the HTML to include JS call stacks in crash reports.
    * Removed legacy unresponsive logging from WindowService.

--- src/main/index.ts
+++ src/main/index.ts
@@ -34,6 +34,16 @@ if (isWin) {
   app.commandLine.appendSwitch('wm-window-animations-disabled')
 }
 
+// Enable features for unresponsive renderer js call stacks
+app.commandLine.appendSwitch('enable-features', 'DocumentPolicyIncludeJSCallStacksInCrashReports')
+app.on('web-contents-created', (_, webContents) => {
+  webContents.on('unresponsive', async () => {
+    // Interrupt execution and collect call stack from unresponsive renderer
+    const callStack = await webContents.mainFrame.collectJavaScriptCallStack()
+    Logger.error('Renderer unresponsive\n', callStack)
+  })
+})
+
 // in production mode, handle uncaught exception and unhandled rejection globally
 if (!isDev) {
   // handle uncaught exception
