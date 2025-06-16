42b69d21633967d639da515823562430c7d58c42
suyao <sy20010504@gmail.com>
2025-06-16 21:41:36 +0800

    refactor: remove externalLiveBindings option from Rollup output configuration in electron.vite.config.ts

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -21,7 +21,6 @@ export default defineConfig({
       rollupOptions: {
         external: ['@libsql/client', 'bufferutil', 'utf-8-validate'],
         output: {
-          externalLiveBindings: false, // 不加启动不了
           // 彻底禁用代码分割 - 返回 null 强制单文件打包
           manualChunks: undefined,
           // 内联所有动态导入，这是关键配置
