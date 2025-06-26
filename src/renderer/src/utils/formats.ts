9b1fb1bc30421a3af724f79eba8bdacbe34ab6ec
MurphyLo <murphylo@mail.bnu.edu.cn>
2025-06-26 14:38:59 +0800

    fix: 修复\[\]块公式在Markdown引用块中渲染失败问题

--- src/renderer/src/utils/formats.ts
+++ src/renderer/src/utils/formats.ts
@@ -54,7 +54,7 @@ export function escapeDollarNumber(text: string) {
 }
 
 export function escapeBrackets(text: string) {
-  const pattern = /(```[\s\S]*?```|`.*?`)|\\\[([\s\S]*?[^\\])\\]|\\\((.*?)\\\)/g
+  const pattern = /(```[\s\S]*?```|`.*?`)|\\\[([\s\S]*?[^\\])\\\]|\\\((.*?)\\\)/g
   return text.replace(pattern, (match, codeBlock, squareBracket, roundBracket) => {
     if (codeBlock) {
       return codeBlock
