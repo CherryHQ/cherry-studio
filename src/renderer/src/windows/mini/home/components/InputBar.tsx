54d1baf71a4369ec9d8a77f9742f2be689cecdb5
qingcuilu <qingcuilu@tencent.com>
2025-07-04 18:40:35 +0800

    build:修复no use错误

--- src/renderer/src/windows/mini/home/components/InputBar.tsx
+++ src/renderer/src/windows/mini/home/components/InputBar.tsx
@@ -1,7 +1,6 @@
 import ModelAvatar from '@renderer/components/Avatar/ModelAvatar'
 import { Assistant } from '@renderer/types'
 import { Input as AntdInput } from 'antd'
-import { InputRef } from 'rc-input/lib/interface'
 import { TextAreaRef } from 'rc-textarea/lib/interface'
 import React, { useRef } from 'react'
 import styled from 'styled-components'
@@ -50,20 +49,10 @@ InputBar.displayName = 'InputBar'
 const InputWrapper = styled.div`
   display: flex;
   margin-top: 10px;
-  
+
   & > :first-child {
     margin-top: 4px;
   }
-`
-
-const Input = styled(AntdInput.TextArea)`
-  background: none;
-  border: none;
-  -webkit-app-region: none;
-  font-size: 18px;
-  resize: none;
-  margin-left: 8px;
-  align-self: flex-start;
 `
 
 export default InputBar
