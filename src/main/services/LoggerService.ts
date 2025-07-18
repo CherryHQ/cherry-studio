84a579ddefe0a4b7cbb32c12caba89c6e00a6a21
fullex <0xfullex@gmail.com>
2025-07-18 12:43:09 +0800

    chore: update ESLint configuration and LoggerService formatting
    - Refactored ESLint configuration to include custom rules for LoggerService, applying them specifically to the src directory while ignoring test and mock files.
    - Commented out date formatting options in LoggerService for improved clarity in development logging.

--- src/main/services/LoggerService.ts
+++ src/main/services/LoggerService.ts
@@ -128,9 +128,9 @@ export class LoggerService {
     if (isDev) {
       const datetimeColored = colorText(
         new Date().toLocaleString('zh-CN', {
-          year: 'numeric',
-          month: '2-digit',
-          day: '2-digit',
+          // year: 'numeric',
+          // month: '2-digit',
+          // day: '2-digit',
           hour: '2-digit',
           minute: '2-digit',
           second: '2-digit',
