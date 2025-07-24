3d15fa845e88799ba574955c135a12e868a5ba49
Konv Suu <2583695112@qq.com>
2025-07-24 17:18:22 +0800

    feat: improve builtin mcp sever display style

--- src/renderer/src/store/mcp.ts
+++ src/renderer/src/store/mcp.ts
@@ -75,6 +75,7 @@ export const builtinMCPServers: MCPServer[] = [
   {
     id: nanoid(),
     name: '@cherry/mcp-auto-install',
+    reference: 'https://docs.cherry-ai.com/advanced-basic/mcp/auto-install',
     getBuiltinDescription: () => i18n.t('settings.mcp.builtinServersDescriptions.mcp_auto_install'),
     type: 'inMemory',
     command: 'npx',
@@ -87,6 +88,8 @@ export const builtinMCPServers: MCPServer[] = [
     name: '@cherry/memory',
     getBuiltinDescription: () => i18n.t('settings.mcp.builtinServersDescriptions.memory'),
     type: 'inMemory',
+    description:
+      '基于本地知识图谱的持久性记忆基础实现。这使得模型能够在不同对话间记住用户的相关信息。需要配置 MEMORY_FILE_PATH 环境变量。https://github.com/modelcontextprotocol/servers/tree/main/src/memory',
     isActive: true,
     env: {
       MEMORY_FILE_PATH: 'YOUR_MEMORY_FILE_PATH'
