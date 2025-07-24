fa4b455c225b0bbbd68d84f29ecd42e280e684a6
suyao <sy20010504@gmail.com>
2025-07-24 15:53:56 +0800

    feat(models): add regex pattern for 'gemma3' model to enhance model compatibility

--- src/renderer/src/config/models.ts
+++ src/renderer/src/config/models.ts
@@ -186,6 +186,7 @@ const visionAllowedModels = [
   'gemma-3(?:-[\\w-]+)',
   'doubao-seed-1[.-]6(?:-[\\w-]+)?',
   'kimi-thinking-preview',
+  `gemma3(?:-[\\w-]+)`,
   'kimi-vl-a3b-thinking(?:-[\\w-]+)?',
   'llama-guard-4(?:-[\\w-]+)?',
   'llama-4(?:-[\\w-]+)?'
