7f2bbec98226e27d6b4b95ea5b1368843eab043b
icarus <eurfelux@gmail.com>
2025-08-03 18:02:13 +0800

    refactor(TranslateSettings): 使用antd Form重构自定义语言模态框表单逻辑
    重构自定义语言模态框，将原有的手动状态管理改为使用antd Form组件管理表单状态
    表单验证逻辑整合到handleSubmit中，提高代码可维护性
    修复emoji显示不同步的问题

--- src/renderer/src/pages/settings/TranslateSettings/CustomLanguageModal.tsx
+++ src/renderer/src/pages/settings/TranslateSettings/CustomLanguageModal.tsx
@@ -4,7 +4,7 @@ import { HStack } from '@renderer/components/Layout'
 import { builtinLangCodeList } from '@renderer/config/translate'
 import { addCustomLanguage, updateCustomLanguage } from '@renderer/services/TranslateService'
 import { CustomTranslateLanguage } from '@renderer/types'
-import { Button, Input, Modal, Popover } from 'antd'
+import { Button, Form, Input, Modal, Popover } from 'antd'
 import { isEmpty } from 'lodash'
 import { FC, useCallback, useEffect, useMemo, useState } from 'react'
 import { useTranslation } from 'react-i18next'
@@ -22,114 +22,124 @@ const logger = loggerService.withContext('CustomLanguageModal')
 
 const CustomLanguageModal = ({ isOpen, editingCustomLanguage, onAdd, onEdit, onCancel }: Props) => {
   const { t } = useTranslation()
-  const [emoji, setEmoji] = useState(editingCustomLanguage?.emoji ?? '🏳️')
-  const [value, setValue] = useState(editingCustomLanguage?.value ?? '')
-  const [langCode, setLangCode] = useState(editingCustomLanguage?.langCode ?? '')
+  const [form] = Form.useForm()
+  // 表单无法同步显示emoji，会出现显示的emoji和表单中实际emoji不一致的情况，所以需要单独管理
+  const defaultEmoji = '🏳️'
+  const [emoji, setEmoji] = useState(defaultEmoji)
+
+  useEffect(() => {
+    if (editingCustomLanguage) {
+      form.setFieldsValue({
+        emoji: editingCustomLanguage.emoji,
+        value: editingCustomLanguage.value,
+        langCode: editingCustomLanguage.langCode
+      })
+      setEmoji(editingCustomLanguage.emoji)
+    } else {
+      form.resetFields()
+      setEmoji(defaultEmoji)
+    }
+  }, [editingCustomLanguage, isOpen, form])
 
   const title = useMemo(
     () => (editingCustomLanguage ? t('common.edit') : t('common.add')) + t('translate.custom.label'),
     [editingCustomLanguage, t]
   )
 
-  const innerCancel = useCallback(() => {
-    onCancel()
-    clearState()
-  }, [onCancel])
+  const handleSubmit = useCallback(
+    async (values: any) => {
+      const { emoji, value, langCode } = values
 
-  // 基本校验 规则暂时硬编码
-  const checkBeforeSubmit = useCallback((): boolean => {
-    logger.debug(`checkBeforeSubmit`)
-    if (isEmpty(value)) {
-      window.message.error(t('settings.translate.custom.error.value.empty'))
-      return false
-    }
-    if (value.length > 32) {
-      window.message.error(t('settings.translate.custom.error.value.too_long'))
-      return false
-    }
-    logger.debug(`checking ${langCode}, result is ${isEmpty(langCode)}`)
-    if (!/^[a-zA-Z]{2,3}(-[a-zA-Z]{2,3})?$/.test(langCode) || isEmpty(langCode)) {
-      window.message.error(t('settings.translate.custom.error.langCode.invalid'))
-      return false
-    }
-    if (builtinLangCodeList.includes(langCode.toLowerCase())) {
-      window.message.error(t('settings.translate.custom.error.langCode.builtin'))
-      return false
-    }
-    return true
-  }, [langCode, t, value])
+      // 基本校验 规则暂时硬编码
+      logger.debug(`checkBeforeSubmit`, {
+        emoji,
+        value,
+        langCode
+      })
+      if (isEmpty(value)) {
+        window.message.error(t('settings.translate.custom.error.value.empty'))
+        return
+      }
+      if (value.length > 32) {
+        window.message.error(t('settings.translate.custom.error.value.too_long'))
+        return
+      }
+      logger.debug(`checking ${langCode}, result is ${isEmpty(langCode)}`)
+      if (!/^[a-zA-Z]{2,3}(-[a-zA-Z]{2,3})?$/.test(langCode) || isEmpty(langCode)) {
+        window.message.error(t('settings.translate.custom.error.langCode.invalid'))
+        return
+      }
+      if (builtinLangCodeList.includes(langCode.toLowerCase())) {
+        window.message.error(t('settings.translate.custom.error.langCode.builtin'))
+        return
+      }
 
-  const handleSubmit = useCallback(async () => {
-    if (!checkBeforeSubmit()) {
-      return
-    }
-    if (editingCustomLanguage) {
-      try {
-        await updateCustomLanguage(editingCustomLanguage, value, emoji, langCode)
-        onEdit({ ...editingCustomLanguage, emoji, value, langCode })
-        window.message.success(t('settings.translate.custom.success.update'))
-      } catch (e) {
-        window.message.error(t('settings.translate.custom.error.update'))
+      if (editingCustomLanguage) {
+        try {
+          await updateCustomLanguage(editingCustomLanguage, value, emoji, langCode)
+          onEdit({ ...editingCustomLanguage, emoji, value, langCode })
+          window.message.success(t('settings.translate.custom.success.update'))
+        } catch (e) {
+          window.message.error(t('settings.translate.custom.error.update'))
+        }
+      } else {
+        try {
+          const added = await addCustomLanguage(value, emoji, langCode)
+          onAdd(added)
+          window.message.success(t('settings.translate.custom.success.add'))
+        } catch (e) {
+          window.message.error(t('settings.translate.custom.error.add'))
+        }
       }
-    } else {
-      try {
-        const added = await addCustomLanguage(value, emoji, langCode)
-        onAdd(added)
-        window.message.success(t('settings.translate.custom.success.add'))
-      } catch (e) {
-        window.message.error(t('settings.translate.custom.error.add'))
-      }
-    }
-    innerCancel()
-  }, [checkBeforeSubmit, editingCustomLanguage, emoji, innerCancel, langCode, onAdd, onEdit, t, value])
+      onCancel()
+    },
+    [editingCustomLanguage, onCancel, t, onEdit, onAdd]
+  )
 
   const footer = useMemo(() => {
     return [
-      <Button key="modal-cancel" onClick={innerCancel}>
+      <Button key="modal-cancel" onClick={onCancel}>
         {t('common.cancel')}
       </Button>,
-      <Button key="modal-save" type="primary" onClick={handleSubmit}>
+      <Button key="modal-save" type="primary" onClick={form.submit}>
         {editingCustomLanguage ? t('common.save') : t('common.add')}
       </Button>
     ]
-  }, [editingCustomLanguage, handleSubmit, innerCancel, t])
-
-  const clearState = () => {
-    // 清空所有状态
-    setValue('')
-    setLangCode('')
-    setEmoji('')
-  }
-
-  useEffect(() => {
-    setValue(editingCustomLanguage?.value ?? '')
-    setLangCode(editingCustomLanguage?.langCode ?? '')
-    setEmoji(editingCustomLanguage?.emoji ?? '')
-  }, [editingCustomLanguage, isOpen])
+  }, [onCancel, t, form.submit, editingCustomLanguage])
 
   return (
-    <Modal open={isOpen} title={title} footer={footer} onCancel={innerCancel}>
-      <HStack alignItems="center" gap={4}>
-        <Popover content={<EmojiPicker onEmojiClick={setEmoji} />} arrow trigger="click">
-          <ButtonContainer>
-            <Button style={{ aspectRatio: '1/1' }} icon={<Emoji emoji={emoji} />}></Button>
-          </ButtonContainer>
-        </Popover>
-        <InputContainer>
-          <Input
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-            placeholder={t('settings.translate.custom.value.placeholder')}
-          />
-        </InputContainer>
-        <InputContainer>
-          <Input
-            value={langCode}
-            onChange={(e) => setLangCode(e.target.value)}
-            placeholder={t('settings.translate.custom.langCode.placeholder')}
-          />
-        </InputContainer>
-      </HStack>
+    <Modal open={isOpen} title={title} footer={footer} onCancel={onCancel}>
+      <Form form={form} onFinish={handleSubmit} layout="vertical">
+        <HStack alignItems="center" gap={4}>
+          <Popover
+            content={
+              <EmojiPicker
+                onEmojiClick={(emoji) => {
+                  form.setFieldsValue({ emoji })
+                  setEmoji(emoji)
+                }}
+              />
+            }
+            arrow
+            trigger="click">
+            <ButtonContainer>
+              <Form.Item name="emoji" noStyle>
+                <Button style={{ aspectRatio: '1/1' }} icon={<Emoji emoji={emoji} />} />
+              </Form.Item>
+            </ButtonContainer>
+          </Popover>
+          <InputContainer>
+            <Form.Item name="value" noStyle>
+              <Input placeholder={t('settings.translate.custom.value.placeholder')} />
+            </Form.Item>
+          </InputContainer>
+          <InputContainer>
+            <Form.Item name="langCode" noStyle>
+              <Input placeholder={t('settings.translate.custom.langCode.placeholder')} />
+            </Form.Item>
+          </InputContainer>
+        </HStack>
+      </Form>
     </Modal>
   )
 }
