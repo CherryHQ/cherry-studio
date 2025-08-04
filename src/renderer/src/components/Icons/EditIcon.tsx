a66d0ae1704488eeccc0d3a394d599abcbc66260
one <wangan.cs@gmail.com>
2025-08-04 14:56:24 +0800

    refactor: use Pencil as EditIcon

--- src/renderer/src/components/Icons/EditIcon.tsx
+++ src/renderer/src/components/Icons/EditIcon.tsx
@@ -1,5 +1,5 @@
-import { Pen } from 'lucide-react'
+import { Pencil } from 'lucide-react'
 
-const EditIcon = (props: React.ComponentProps<typeof Pen>) => <Pen size="1rem" {...props} />
+const EditIcon = (props: React.ComponentProps<typeof Pencil>) => <Pencil size="1rem" {...props} />
 
 export default EditIcon
