2d9ed37cadbbda54380f05b84011e5785b891aa5
icarus <eurfelux@gmail.com>
2025-08-08 07:03:59 +0800

    refactor(Tags): 移除FreeTag组件中未使用的showLabel属性

--- src/renderer/src/components/Tags/Model/FreeTag.tsx
+++ src/renderer/src/components/Tags/Model/FreeTag.tsx
@@ -5,19 +5,16 @@ import CustomTag, { CustomTagProps } from '../CustomTag'
 type Props = {
   size?: number
   showTooltip?: boolean
-  showLabel?: boolean
 } & Omit<CustomTagProps, 'size' | 'tooltip' | 'icon' | 'color' | 'children'>
 
-export const FreeTag = ({ size, showTooltip, showLabel, ...restProps }: Props) => {
+export const FreeTag = ({ size, showTooltip, ...restProps }: Props) => {
   const { t } = useTranslation()
   return (
     <CustomTag
       size={size}
       color="#7cb305"
       icon={t('models.type.free')}
-      tooltip={showTooltip ? t('models.type.reasoning') : undefined}
-      {...restProps}>
-      {showLabel ? t('models.type.reasoning') : ''}
-    </CustomTag>
+      tooltip={showTooltip ? t('models.type.free') : undefined}
+      {...restProps}></CustomTag>
   )
 }
