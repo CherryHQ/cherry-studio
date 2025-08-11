037d132c6e178d7198059d6e0add4e757f9f7265
icarus <eurfelux@gmail.com>
2025-08-12 00:31:39 +0800

    Revert "remove ant splitter panel scrollbar"
    This reverts commit 947fe30402c84ca169278242ce2a1d41d559978b.

--- src/renderer/src/pages/settings/ProviderSettings/index.tsx
+++ src/renderer/src/pages/settings/ProviderSettings/index.tsx
@@ -456,7 +456,7 @@ const ProvidersList: FC = () => {
   return (
     <Container className="selectable">
       <Splitter>
-        <Splitter.Panel min={250} defaultSize={250} style={{ overflow: 'auto', scrollbarWidth: 'none' }}>
+        <Splitter.Panel min={250} defaultSize={250}>
           <ProviderListContainer>
             <AddButtonWrapper>
               <Input
@@ -522,7 +522,7 @@ const ProvidersList: FC = () => {
           </ProviderListContainer>
         </Splitter.Panel>
 
-        <Splitter.Panel min={'50%'} style={{ overflow: 'auto', scrollbarWidth: 'none' }}>
+        <Splitter.Panel min={'50%'}>
           <ProviderSetting providerId={selectedProvider.id} key={selectedProvider.id} />
         </Splitter.Panel>
       </Splitter>
