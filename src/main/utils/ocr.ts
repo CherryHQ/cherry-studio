fa035e3ee430bf504de29d49f3176cf2fa8e2f72
beyondkmp <beyondkmp@gmail.com>
2025-08-25 10:42:55 +0800

    improve image preprocess

--- src/main/utils/ocr.ts
+++ src/main/utils/ocr.ts
@@ -2,10 +2,14 @@ import { ImageFileMetadata } from '@types'
 import { readFile } from 'fs/promises'
 import sharp from 'sharp'
 
-const preprocessImage = (buffer: Buffer) => {
-  // threshold 70 is hard-encoded
-  const result = sharp(buffer).greyscale().normalise().threshold(70).toBuffer()
-  return result
+const preprocessImage = async (buffer: Buffer) => {
+  return await sharp(buffer)
+    .grayscale() // 转为灰度
+    .normalize()
+    .sharpen()
+    .threshold(100) // 可能需要根据具体图片调整
+    .png({ quality: 100 })
+    .toBuffer()
 }
 
 /**
@@ -20,9 +24,6 @@ const preprocessImage = (buffer: Buffer) => {
  * 3. 后续可扩展其他预处理步骤
  */
 export const loadOcrImage = async (file: ImageFileMetadata): Promise<Buffer> => {
-  // 读取原始图像
   const buffer = await readFile(file.path)
-
-  // 统一预处理流程
-  return preprocessImage(buffer)
+  return await preprocessImage(buffer)
 }
