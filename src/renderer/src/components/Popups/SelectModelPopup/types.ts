3d3a61e52651d4415e7e0970ceb32df635dc5f61
one <wangan.cs@gmail.com>
2025-08-26 22:18:03 +0800

    refactor: more comments

--- src/renderer/src/components/Popups/SelectModelPopup/types.ts
+++ src/renderer/src/components/Popups/SelectModelPopup/types.ts
@@ -1,13 +1,19 @@
 import { Model } from '@renderer/types'
 import { ReactNode } from 'react'
 
-// 列表项类型，组名也作为列表项
-export type ListItemType = 'group' | 'model'
-
-// 滚动触发来源类型
+/**
+ * 滚动触发来源类型
+ */
 export type ScrollTrigger = 'initial' | 'search' | 'keyboard' | 'none'
 
-// 扁平化列表项
+/**
+ * 列表项分类，组名也作为列表项
+ */
+export type ListItemType = 'group' | 'model'
+
+/**
+ * 扁平化列表项基础类型
+ */
 export type FlatListBaseItem = {
   key: string
   type: ListItemType
@@ -16,11 +22,17 @@ export type FlatListBaseItem = {
   isSelected?: boolean
 }
 
+/**
+ * 模型分组列表项
+ */
 export type FlatListGroup = FlatListBaseItem & {
   type: 'group'
   actions?: ReactNode
 }
 
+/**
+ * 模型列表项
+ */
 export type FlatListModel = FlatListBaseItem & {
   type: 'model'
   model: Model
@@ -28,4 +40,7 @@ export type FlatListModel = FlatListBaseItem & {
   isPinned?: boolean
 }
 
+/**
+ * 扁平化列表项
+ */
 export type FlatListItem = FlatListGroup | FlatListModel
