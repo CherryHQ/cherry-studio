782065f21d665a2cbb85ccfc4c47b0197ed7cf4e
beyondkmp <beyondkmp@gmail.com>
2025-08-30 19:58:38 +0800

    update

--- src/main/services/SelectionService.ts
+++ src/main/services/SelectionService.ts
@@ -416,7 +416,6 @@ export class SelectionService {
       hasShadow: false,
       thickFrame: false,
       roundedCorners: true,
-      backgroundMaterial: 'auto',
 
       // Platform specific settings
       //   [macOS] DO NOT set focusable to false, it will make other windows bring to front together
