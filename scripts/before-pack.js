f4804ce3582f6c2edbcee908000bb170febd3f65
beyondkmp <beyondkmp@gmail.com>
2025-09-01 11:35:38 +0800

    refactor: streamline filter logic for architecture-specific packages in before-pack script
    - Consolidated platform-specific filter conditions for arm64 and x64 architectures.
    - Improved readability and maintainability by using arrays to manage filters for different architectures.
    - Ensured that the correct filters are applied based on the architecture during the packaging process.

--- scripts/before-pack.js
+++ scripts/before-pack.js
@@ -1,60 +1,52 @@
 const { Arch } = require('electron-builder')
 
 exports.default = async function (context) {
-  const platform = context.packager.platform.name
   const arch = context.arch
   let filters = context.packager.config.files[0].filter
 
-  if (platform === 'mac') {
-    if (arch === Arch.arm64) {
-      filters.push(
-        '!node_modules/@img/sharp-darwin-x64',
-        '!node_modules/@img/sharp-libvips-darwin-x64',
-        '!node_modules/@napi-rs/system-ocr-darwin-x64',
-        '!node_modules/@libsql/darwin-x64'
-      )
-    } else {
-      filters.push(
-        '!node_modules/@img/sharp-darwin-arm64',
-        '!node_modules/@img/sharp-libvips-darwin-arm64',
-        '!node_modules/@napi-rs/system-ocr-darwin-arm64',
-        '!node_modules/@libsql/darwin-arm64'
-      )
-    }
-  }
+  const allArm64 = [
+    '!node_modules/@img/sharp-darwin-arm64',
+    '!node_modules/@img/sharp-linux-arm64',
+    '!node_modules/@img/sharp-win32-arm64',
 
-  if (platform === 'linux') {
-    if (arch === Arch.arm64) {
-      filters.push(
-        '!node_modules/@img/sharp-libvips-linux-x64',
-        '!node_modules/@img/sharp-linux-x64',
-        '!node_modules/@libsql/linux-x64-gnu',
-        '!node_modules/@libsql/linux-x64-musl'
-      )
-    } else {
-      filters.push(
-        '!node_modules/@img/sharp-libvips-linux-arm64',
-        '!node_modules/@img/sharp-linux-arm64',
-        '!node_modules/@libsql/linux-arm64-gnu',
-        '!node_modules/@libsql/linux-arm64-musl'
-      )
-    }
-  }
+    '!node_modules/@img/sharp-libvips-darwin-arm64',
+    '!node_modules/@img/sharp-libvips-linux-arm64',
+    '!node_modules/@img/sharp-libvips-win32-arm64',
 
-  if (platform === 'windows') {
-    if (arch === Arch.arm64) {
-      filters.push(
-        '!node_modules/@img/sharp-win32-arm64',
-        '!node_modules/@img/sharp-libvips-win32-arm64',
-        '!node_modules/@libsql/win32-arm64-msvc',
-        '!node_modules/@strongtz/win32-arm64-msvc'
-      )
-    } else {
-      filters.push(
-        '!node_modules/@img/sharp-win32-x64',
-        '!node_modules/@img/sharp-libvips-win32-x64',
-        '!node_modules/@libsql/win32-x64-msvc'
-      )
-    }
+    '!node_modules/@napi-rs/system-ocr-darwin-arm64',
+    '!node_modules/@napi-rs/system-ocr-win32-arm64',
+
+    '!node_modules/@libsql/darwin-arm64',
+    '!node_modules/@libsql/linux-arm64-gnu',
+    '!node_modules/@libsql/linux-arm64-musl',
+    '!node_modules/@napi-rs/system-ocr-linux-arm64'
+  ]
+
+  const allX64 = [
+    '!node_modules/@img/sharp-darwin-x64',
+    '!node_modules/@img/sharp-win32-x64',
+    '!node_modules/@img/sharp-linux-x64',
+
+    '!node_modules/@img/sharp-libvips-darwin-x64',
+    '!node_modules/@img/sharp-libvips-win32-x64',
+    '!node_modules/@img/sharp-libvips-linux-x64',
+
+    '!node_modules/@napi-rs/system-ocr-darwin-x64',
+    '!node_modules/@napi-rs/system-ocr-win32-x64',
+
+    '!node_modules/@libsql/darwin-x64',
+    '!node_modules/@libsql/win32-x64-msvc',
+    '!node_modules/@libsql/linux-x64-gnu',
+    '!node_modules/@libsql/linux-x64-musl'
+  ]
+
+  if (arch === Arch.arm64) {
+    // remove all x64 filters
+    filters = filters.filter((filter) => !allX64.includes(filter))
+    filters.push(...allArm64)
+  } else {
+    // remove all arm64 filters
+    filters = filters.filter((filter) => !allArm64.includes(filter))
+    filters.push(...allX64)
   }
 }
