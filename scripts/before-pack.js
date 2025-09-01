f7632134e0a83ca2b54fab35da099f3d89148119
beyondkmp <beyondkmp@gmail.com>
2025-09-01 14:36:26 +0800

    refactor: enhance architecture-specific package management in before-pack script
    - Updated the before-pack script to include additional architecture-specific packages for arm64 and x64.
    - Improved the organization of package filters and streamlined the logic for handling different architectures during the packaging process.
    - Ensured that the correct filters are applied based on the architecture, enhancing the build process for various platforms.

--- scripts/before-pack.js
+++ scripts/before-pack.js
@@ -1,54 +1,56 @@
 const { Arch } = require('electron-builder')
 const { downloadNpmPackage } = require('./utils')
 
+// if you want to add new packages with different architectures, you can add them here
+// please add to allX64 and allArm64 from yarn.lock
+const allArm64 = {
+  '@img/sharp-darwin-arm64': '0.34.3',
+  '@img/sharp-win32-arm64': '0.34.3',
+  '@img/sharp-linux-arm64': '0.34.3',
+  '@img/sharp-linuxmusl-arm64': '0.34.3',
+
+  '@img/sharp-libvips-darwin-arm64': '1.2.0',
+  '@img/sharp-libvips-linux-arm64': '1.2.0',
+  '@img/sharp-libvips-linuxmusl-arm64': '1.2.0',
+
+  '@libsql/darwin-arm64': '0.4.7',
+  '@libsql/linux-arm64-gnu': '0.4.7',
+  '@libsql/linux-arm64-musl': '0.4.7',
+  '@strongtz/win32-arm64-msvc': '0.4.7',
+
+  '@napi-rs/system-ocr-darwin-arm64': '1.0.2',
+  '@napi-rs/system-ocr-win32-arm64-msvc': '1.0.2'
+}
+
+const allX64 = {
+  '@img/sharp-darwin-x64': '0.34.3',
+  '@img/sharp-linux-x64': '0.34.3',
+  '@img/sharp-linuxmusl-x64': '0.34.3',
+  '@img/sharp-win32-x64': '0.34.3',
+
+  '@img/sharp-libvips-darwin-x64': '1.2.0',
+  '@img/sharp-libvips-linux-x64': '1.2.0',
+  '@img/sharp-libvips-linuxmusl-x64': '1.2.0',
+
+  '@libsql/darwin-x64': '0.4.7',
+  '@libsql/linux-x64-gnu': '0.4.7',
+  '@libsql/linux-x64-musl': '0.4.7',
+  '@libsql/win32-x64-msvc': '0.4.7',
+
+  '@napi-rs/system-ocr-darwin-x64': '1.0.2',
+  '@napi-rs/system-ocr-win32-x64-msvc': '1.0.2'
+}
+
+const platformToArch = {
+  mac: 'darwin',
+  windows: 'win32',
+  linux: 'linux'
+}
+
 exports.default = async function (context) {
   const arch = context.arch
   const platform = context.packager.platform.name
   let filters = context.packager.config.files[0].filter
-
-  const platformToArch = {
-    mac: 'darwin',
-    windows: 'win32',
-    linux: 'linux'
-  }
-
-  const allArm64 = {
-    '@img/sharp-darwin-arm64': '0.34.3',
-    '@img/sharp-win32-arm64': '0.34.3',
-    '@img/sharp-linux-arm64': '0.34.3',
-    '@img/sharp-linuxmusl-arm64': '0.34.3',
-
-    '@img/sharp-libvips-darwin-arm64': '1.2.0',
-    '@img/sharp-libvips-linux-arm64': '1.2.0',
-    '@img/sharp-libvips-linuxmusl-arm64': '1.2.0',
-
-    '@libsql/darwin-arm64': '0.4.7',
-    '@libsql/linux-arm64-gnu': '0.4.7',
-    '@libsql/linux-arm64-musl': '0.4.7',
-    '@strongtz/win32-arm64-msvc': '0.4.7',
-
-    '@napi-rs/system-ocr-darwin-arm64': '1.0.2',
-    '@napi-rs/system-ocr-win32-arm64-msvc': '1.0.2'
-  }
-
-  const allX64 = {
-    '@img/sharp-darwin-x64': '0.34.3',
-    '@img/sharp-linux-x64': '0.34.3',
-    '@img/sharp-linuxmusl-x64': '0.34.3',
-    '@img/sharp-win32-x64': '0.34.3',
-
-    '@img/sharp-libvips-darwin-x64': '1.2.0',
-    '@img/sharp-libvips-linux-x64': '1.2.0',
-    '@img/sharp-libvips-linuxmusl-x64': '1.2.0',
-
-    '@libsql/darwin-x64': '0.4.7',
-    '@libsql/linux-x64-gnu': '0.4.7',
-    '@libsql/linux-x64-musl': '0.4.7',
-    '@libsql/win32-x64-msvc': '0.4.7',
-
-    '@napi-rs/system-ocr-darwin-x64': '1.0.2',
-    '@napi-rs/system-ocr-win32-x64-msvc': '1.0.2'
-  }
 
   const arm64Filters = Object.keys(allArm64).map((f) => '!node_modules/' + f)
   const x64Filters = Object.keys(allX64).map((f) => '!node_modules/' + f)
@@ -68,16 +70,19 @@ exports.default = async function (context) {
   const changeFilters = (archs, addFilters, deleteFilters) => {
     console.log('downloading all ' + arch + ' packages...')
     downloadPackages(archs)
-
-    // remove all x64 filters
-
+    // remove all different architectures filters
     filters = filters.filter((filter) => !deleteFilters.includes(filter))
+    // add the same architectures filters
     filters.push(...addFilters)
   }
 
   if (arch === Arch.arm64) {
     changeFilters(allArm64, arm64Filters, x64Filters)
-  } else {
+    return
+  }
+
+  if (arch === Arch.x64) {
     changeFilters(allX64, x64Filters, arm64Filters)
+    return
   }
 }
