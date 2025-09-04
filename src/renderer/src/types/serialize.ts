17ef8719e098be7ad4a02b046fac3338aba185ae
icarus <eurfelux@gmail.com>
2025-09-04 13:35:28 +0800

    Revert "refactor(serialize): 重命名 SerializableValue 为 SerializablePrimitive 以提高可读性"
    This reverts commit 8408a8d3597ee66b557a80ac65cf85be2653c922.

--- src/renderer/src/types/serialize.ts
+++ src/renderer/src/types/serialize.ts
@@ -1,6 +1,6 @@
-export type Serializable = SerializablePrimitive | { [key: string]: SerializablePrimitive } | SerializablePrimitive[]
+export type Serializable = null | boolean | number | string | { [key: string]: SerializableValue } | SerializableValue[]
 
-type SerializablePrimitive = null | boolean | number | string /**
+type SerializableValue = null | boolean | number | string | { [key: string]: any } | any[] /**
  * 判断一个值是否可序列化（适合用于 Redux 状态）
  * 支持嵌套对象、数组的深度检测
  */
