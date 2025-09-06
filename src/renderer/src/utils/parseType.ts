c6df31244b66653a8c46f8842020e7abc1a24574
RieN 7z <zrien7@gmail.com>
2025-09-07 04:42:02 +0800

    fix: move `parseFileTypes` into utils/file

--- src/renderer/src/utils/parseType.ts
+++
@@ -1,8 +0,0 @@
-import { FileTypes } from '@renderer/types'
-
-export function parseFileTypes(str: string): FileTypes | null {
-  if (Object.values(FileTypes).includes(str as FileTypes)) {
-    return str as FileTypes
-  }
-  return null
-}
