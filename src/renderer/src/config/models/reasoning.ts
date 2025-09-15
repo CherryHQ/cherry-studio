992b237e5d6f8f9fa5d90de091189891362344cb
Yicheng <suyicheng1995@gmail.com>
2025-09-15 03:37:48 +0000

    add qwen-plus new model

--- src/renderer/src/config/models/reasoning.ts
+++ src/renderer/src/config/models/reasoning.ts
@@ -415,6 +415,7 @@ export const THINKING_TOKEN_MAP: Record<string, { min: number; max: number }> = 
   'qwen3-235b-a22b-thinking-2507$': { min: 0, max: 81_920 },
   'qwen3-30b-a3b-thinking-2507$': { min: 0, max: 81_920 },
   'qwen-plus-2025-07-28$': { min: 0, max: 81_920 },
+  'qwen-plus-2025-09-11$': { min: 0, max: 81_920 },
   'qwen-plus-latest$': { min: 0, max: 81_920 },
   'qwen3-1\\.7b$': { min: 0, max: 30_720 },
   'qwen3-0\\.6b$': { min: 0, max: 30_720 },
