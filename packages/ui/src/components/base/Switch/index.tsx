52e2df59e9ca4967b431c5bfe1e73b5f820e4072
icarus <eurfelux@gmail.com>
2025-09-17 22:12:10 +0800

    refactor(Switch): simplify props type definition for loading state
    Remove complex union type in favor of simpler interface extending SwitchProps

--- packages/ui/src/components/base/Switch/index.tsx
+++ packages/ui/src/components/base/Switch/index.tsx
@@ -2,22 +2,9 @@ import type { SwitchProps } from '@heroui/react'
 import { Spinner, Switch } from '@heroui/react'
 
 // Enhanced Switch component with loading state support
-// FIXME: Implementing the loading animation requires the thumbIcon property.
-//        When isLoading is selected, external overriding of this style should not be allowed.
-//        This approach is relatively simple to implement, but it lacks some flexibility.
-type CustomSwitchProps =
-  | (SwitchProps & {
-      isLoading: boolean
-      thumbIcon?: never
-    })
-  | (SwitchProps & {
-      isLoading?: never
-      thumbIcon: SwitchProps['thumbIcon']
-    })
-  | (SwitchProps & {
-      isLoading?: never
-      thumbIcon?: never
-    })
+interface CustomSwitchProps extends SwitchProps {
+  isLoading?: boolean
+}
 
 const CustomizedSwitch = ({ isLoading, children, ref, thumbIcon, ...props }: CustomSwitchProps) => {
   const finalThumbIcon = isLoading ? <Spinner size="sm" /> : thumbIcon
