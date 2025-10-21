eba3b32af798ecaa4367d41440a8e618efd35f8a
icarus <eurfelux@gmail.com>
2025-10-21 22:04:12 +0800

    refactor(AddButton): simplify component by removing FC type and inline props
    Remove unnecessary FC type declaration and inline the Props interface with ButtonProps. Also clean up prop spreading by moving it to the end of the component.

--- src/renderer/src/pages/home/Tabs/components/AddButton.tsx
+++ src/renderer/src/pages/home/Tabs/components/AddButton.tsx
@@ -1,21 +1,15 @@
 import { Button, ButtonProps, cn } from '@heroui/react'
 import { PlusIcon } from 'lucide-react'
-import { FC } from 'react'
 
-interface Props extends ButtonProps {
-  children: React.ReactNode
-}
-
-const AddButton: FC<Props> = ({ children, className, ...props }) => {
+const AddButton = ({ children, className, ...props }: ButtonProps) => {
   return (
     <Button
-      {...props}
-      onPress={props.onPress}
       className={cn(
         'h-9 w-[calc(var(--assistants-width)-20px)] justify-start rounded-lg bg-transparent px-3 text-[13px] text-[var(--color-text-2)] hover:bg-[var(--color-list-item)]',
         className
       )}
-      startContent={<PlusIcon size={16} className="shrink-0" />}>
+      startContent={<PlusIcon size={16} className="shrink-0" />}
+      {...props}>
       {children}
     </Button>
   )
