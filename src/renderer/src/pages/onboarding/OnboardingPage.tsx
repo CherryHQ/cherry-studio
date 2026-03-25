a24b7e2c5e6f53ae42e5f73c415847182f9c339c
kangfenmao <kangfenmao@qq.com>
2026-03-25 11:10:48 +0800

    refactor: remove unused translation hooks and clean up CherryINSettings component

--- src/renderer/src/pages/onboarding/OnboardingPage.tsx
+++ src/renderer/src/pages/onboarding/OnboardingPage.tsx
@@ -10,9 +10,7 @@ const OnboardingPage: FC = () => {
 
   return (
     <div className="flex h-screen w-screen flex-col">
-      {/* Header 区域 - 可拖动 */}
       <div className="drag w-full shrink-0" style={{ height: 'var(--navbar-height)' }} />
-      {/* Content 区域 - 带圆角的卡片，左右下有边距 */}
       <div className="flex flex-1 px-2 pb-2">
         <div className="relative flex flex-1 overflow-hidden rounded-xl bg-(--color-background)">
           <SkipButton />
