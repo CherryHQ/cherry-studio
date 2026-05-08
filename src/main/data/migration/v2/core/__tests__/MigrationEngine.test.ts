7fd7f123914c551292ada467d76ee69602a430eb
jidan745le <420511176@qq.com>
2026-05-08 13:25:27 +0800

    test(migration): update cleared table assertion
    Signed-off-by: jidan745le <420511176@qq.com>

--- src/main/data/migration/v2/core/__tests__/MigrationEngine.test.ts
+++ src/main/data/migration/v2/core/__tests__/MigrationEngine.test.ts
@@ -141,7 +141,7 @@ describe('MigrationEngine', () => {
     await (engine as any).verifyAndClearNewTables()
 
     expect(transactionFn).toHaveBeenCalledTimes(1)
-    expect(deleteFn).toHaveBeenCalledTimes(25)
+    expect(deleteFn).toHaveBeenCalledTimes(db.select.mock.calls.length)
     expect(db).not.toHaveProperty('delete')
   })
 })
