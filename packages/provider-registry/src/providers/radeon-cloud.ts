993f82bfce4e4b16927a59a8625b67e70fa1f085
kangletian <letian.kang@amd.com>
2026-07-16 12:19:29 +0800

    fix(provider): correct AMD GPU Cloud API URL
    Signed-off-by: kangletian <letian.kang@amd.com>

--- packages/provider-registry/src/providers/radeon-cloud.ts
+++ packages/provider-registry/src/providers/radeon-cloud.ts
@@ -3,7 +3,7 @@ import { openaiCompatible } from './types'
 export default openaiCompatible({
   id: 'radeon-cloud',
   name: 'AMD GPU Cloud',
-  baseUrl: 'https://developer.amd.com.cn/radeon/api/v1',
+  baseUrl: 'https://developer.amd.com.cn/radeon/v1',
   website: {
     apiKey: 'https://developer.amd.com.cn/radeon/modelapis',
     docs: 'https://developer.amd.com.cn/radeon/',
