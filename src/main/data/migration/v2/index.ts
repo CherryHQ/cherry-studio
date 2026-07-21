f5ee4ef2dba5e8b5cb787712ecc93be8f361118c
gujiaming <52187003+AtomsH4@users.noreply.github.com>
2026-07-21 22:29:21 +0800

    refactor(migration-diagnostics): narrow native entry point
    Signed-off-by: gujiaming <52187003+AtomsH4@users.noreply.github.com>

--- src/main/data/migration/v2/index.ts
+++ src/main/data/migration/v2/index.ts
@@ -41,22 +41,7 @@ export {
 export { getAllMigrators } from './migrators/migratorRegistry'
 
 // Window management
-export {
-  createMigrationDiagnosticSavedDetail,
-  type MigrationDiagnosticFailureDialog,
-  type MigrationDiagnosticFailurePresentation,
-  presentMigrationDiagnosticFailure,
-  saveMigrationDiagnosticBundleWithDialog
-} from './window/migrationDiagnosticDialogs'
-export {
-  createMigrationDiagnosticEmailUrl,
-  MIGRATION_DIAGNOSTIC_SUPPORT_EMAIL
-} from './window/migrationDiagnosticEmail'
-export {
-  createMigrationDiagnosticNativeI18n,
-  type MigrationDiagnosticNativeI18n,
-  type MigrationDiagnosticNativeLocale
-} from './window/migrationDiagnosticNativeI18n'
+export { presentMigrationDiagnosticFailure } from './window/migrationDiagnosticDialogs'
 export {
   registerMigrationIpcHandlers,
   resetMigrationData,
