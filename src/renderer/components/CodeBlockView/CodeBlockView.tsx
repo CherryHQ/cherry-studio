edb46654b87ba8584c8f2a3b5f30e1daf6e474bb
kkjz <253839323@sohu.com>
2026-08-19 11:07:56 +0800

    fix(CodeBlockView): update split-view-wrapper for responsive layout adjustments
    Signed-off-by: kkjz <253839323@sohu.com>

--- src/renderer/components/CodeBlockView/CodeBlockView.tsx
+++ src/renderer/components/CodeBlockView/CodeBlockView.tsx
@@ -395,7 +395,7 @@ export const CodeBlockView: React.FC<Props> = memo((props) => {
     return (
       <div
         className={cn(
-          'split-view-wrapper flex *:w-full *:flex-[1_1_auto]',
+          'split-view-wrapper flex *:min-w-0 *:flex-1',
           !hasStatusBar && (showSpecialView && !showSourceView ? 'rounded-lg' : 'rounded-b-lg'),
           !hasStatusBar && '[&_.code-viewer]:rounded-[inherit]',
           showSpecialView &&
