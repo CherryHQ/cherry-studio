75a1cee076d9cd576bf7e78fc29451f0d866d134
gujiaming <52187003+AtomsH4@users.noreply.github.com>
2026-09-22 18:56:14 +0800

    fix(mini-apps): align add dialog width
    Signed-off-by: gujiaming <52187003+AtomsH4@users.noreply.github.com>

--- src/renderer/pages/miniApps/NewMiniAppPanel.tsx
+++ src/renderer/pages/miniApps/NewMiniAppPanel.tsx
@@ -253,7 +253,7 @@ const NewMiniAppPanel: FC<Props> = ({ open, app, onClose }) => {
 
   return (
     <Dialog open={open} onOpenChange={handleOpenChange}>
-      <DialogContent closeOnOverlayClick={false} aria-describedby={undefined} className="sm:max-w-md">
+      <DialogContent closeOnOverlayClick={false} aria-describedby={undefined} size="lg">
         <DialogHeader>
           <DialogTitle>{t(isEditing ? 'settings.miniApps.custom.edit_title' : 'miniApp.add.title')}</DialogTitle>
         </DialogHeader>
