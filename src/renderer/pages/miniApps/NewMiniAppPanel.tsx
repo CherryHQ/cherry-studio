e891d82a2cfb73ea9cfdcbd7b5b6a26ea7f37ba8
gujiaming <52187003+AtomsH4@users.noreply.github.com>
2026-09-22 19:43:58 +0800

    fix(mini-apps): use standard add dialog width
    Signed-off-by: gujiaming <52187003+AtomsH4@users.noreply.github.com>

--- src/renderer/pages/miniApps/NewMiniAppPanel.tsx
+++ src/renderer/pages/miniApps/NewMiniAppPanel.tsx
@@ -253,7 +253,7 @@ const NewMiniAppPanel: FC<Props> = ({ open, app, onClose }) => {
 
   return (
     <Dialog open={open} onOpenChange={handleOpenChange}>
-      <DialogContent closeOnOverlayClick={false} aria-describedby={undefined} size="lg">
+      <DialogContent closeOnOverlayClick={false} aria-describedby={undefined}>
         <DialogHeader>
           <DialogTitle>{t(isEditing ? 'settings.miniApps.custom.edit_title' : 'miniApp.add.title')}</DialogTitle>
         </DialogHeader>
